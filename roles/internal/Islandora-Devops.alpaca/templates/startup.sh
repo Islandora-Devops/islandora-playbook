144781d382666a66b178169c41a8719c50b0bc0c
Brian Gregg <bdgregg@pitt.edu>
2024-01-10 11:53:31 -0500

    Draft changes to support RHEL.

--- roles/internal/Islandora-Devops.alpaca/templates/startup.sh
+++ roles/internal/Islandora-Devops.alpaca/templates/startup.sh
@@ -1,15 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ -z "$JAVA_HOME" ]; then
-  if [ -d "/usr/lib/jvm/java-11-openjdk-amd64" ]; then
-    # Make a guess because this is only for Ubuntu
-    export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-  else
-    echo "Cannot locate JAVA, please define JAVA_HOME."
-    exit 1
-  fi
+#if [ -z "$JAVA_HOME" ]; then
+#  if [ -d "/usr/lib/jvm/java-11-openjdk-amd64" ]; then
+#    # Make a guess because this is only for Ubuntu
+#    export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+#  else
+#    echo "Cannot locate JAVA, please define JAVA_HOME."
+#    exit 1
+#  fi
+#fi
+
+if [ "$(grep -Ei 'debian/buntu/mint' /etc/*release)" ]; then
+	if [ -d "/usr/lib/jvm/java-11-openjdk-amd64" ]; then
+	  # Make a guess for Debian based systems.
+      export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+	fi
+elif [ "$(grep -Ei 'fedora|redhat' /etc/*release)" ]; then
+	if [ -d "/usr/lib/jvm/jre" ] || [ -f "/usr/lib/jvm/jre" ] || [ -L "/usr/lib/jvm/jre" ]; then
+	  # Make a guess for RHEL based systems.
+	  export JAVA_HOME=/usr/lib/jvm/jre
+	fi
+else
+	echo "Cannot locate JAVA, please define JAVA_HOME."
 fi
+
+
 # Source the config file.
-. ${ALPACA_HOME}/${ALPACA_CONFIG}
+source ${ALPACA_HOME}/${ALPACA_CONFIG}
 
 ${JAVA_HOME}/bin/java -Dislandora.alpaca.log=${ALPACA_LOG_LEVEL} -Xmx${ALPACA_HEAP} -jar ${ALPACA_JAR} -c ${ALPACA_PROPERTIES} > ${ALPACA_LOG_DIR}/alpaca.log &
