ae9c75952c8cb4a980e7d3f3052cd8897b5e4cf9
Brian Gregg <bdgregg@pitt.edu>
2024-01-11 10:37:35 -0500

    Additional Cleanup - alpaca minor changes to templates/startup.sh.

--- roles/internal/Islandora-Devops.alpaca/templates/startup.sh
+++ roles/internal/Islandora-Devops.alpaca/templates/startup.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-if [ "$(grep -Ei 'debian/buntu/mint' /etc/*release)" ]; then
-	if [ -d "/usr/lib/jvm/java-11-openjdk-amd64" ]; then
-	  # Make a guess for Debian based systems.
-      export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+if [ -z "$JAVA_HOME" ]; then
+	if [ "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]; then
+		if [ -d "/usr/lib/jvm/java-11-openjdk-amd64" ]; then
+	  	# Make a guess for Debian based systems.
+      	export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+		fi
+	elif [ "$(grep -Ei 'fedora|redhat' /etc/*release)" ]; then
+		if [ -d "/usr/lib/jvm/jre" ] || [ -f "/usr/lib/jvm/jre" ] || [ -L "/usr/lib/jvm/jre" ]; then
+	  	# Make a guess for RHEL based systems.
+	  	export JAVA_HOME=/usr/lib/jvm/jre
+		fi
+	else
+		echo "Cannot locate JAVA, please define JAVA_HOME."
 	fi
-elif [ "$(grep -Ei 'fedora|redhat' /etc/*release)" ]; then
-	if [ -d "/usr/lib/jvm/jre" ] || [ -f "/usr/lib/jvm/jre" ] || [ -L "/usr/lib/jvm/jre" ]; then
-	  # Make a guess for RHEL based systems.
-	  export JAVA_HOME=/usr/lib/jvm/jre
-	fi
-else
-	echo "Cannot locate JAVA, please define JAVA_HOME."
 fi
-
 
 # Source the config file.
 source ${ALPACA_HOME}/${ALPACA_CONFIG}
